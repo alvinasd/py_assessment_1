fd27bc15ec636b8982d0b4033380ac1371eb780d
Alvin Lee <a3171998@gmail.com>
2022-06-16 21:08:03 -0700

    update with finished product

--- read_file.py
+++ read_file.py
@@ -10,10 +10,13 @@ csv_data = csv.reader(data)
 
 #reformat data
 rand_data = list(csv_data)
-print(rand_data)
 
-#connect to mysql
-connection = mysql.connect(host = "localhost", user = "root", password = "3171998Aa")
+#put csv data into a DF 
+df = pd.DataFrame(rand_data)
+df.rename(columns={0: 'id', 1: 'first_name', 2: 'last_name'}, inplace=True)
+
+#connect to mysql, password deleted for privacy
+connection = mysql.connect(host = "localhost", user = "root", password = "")
 
 cursor = connection.cursor()
 #select DB, drop table if made, create table
@@ -21,8 +24,13 @@ cursor.execute("USE py_assessment_1;")
 cursor.execute("DROP TABLE IF EXISTS random_data;")
 cursor.execute("CREATE TABLE random_data(id VARCHAR(255), first_name VARCHAR(255), last_name VARCHAR(255));")
 
-row = list["","",""]
-entry = "INSERT INTO random_data VALUES ('%s', '%s', '%s');"
-cursor.execute(entry, list(row))
+#insert DF into mysql table
+for i, row in df.iterrows():
+	query = "INSERT INTO random_data (id, first_name, last_name) VALUES (%s,%s,%s);"
+	cursor.execute(query, tuple(row))
+
 connection.commit()
-print(cursor.execute("SELECT * FROM random_data;"))
+
+#query to transpose columns into individual rows by id
+query = "SELECT id, first_name AS name FROM random_data UNION ALL SELECT id, last_name AS name FROM random_data ORDER BY id;"
+cursor.execute(query)
